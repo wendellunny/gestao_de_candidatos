94a6bbf10236d2341665bd905612caf15fb49b7e
wendellunny <wendelferreiraportugal@gmail.com>
2022-06-14 21:03:52 -0300

    Configurando model de experiencia profissional

--- app/Models/ProfessionalExperience.php
+++ app/Models/ProfessionalExperience.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class ProfessionalExperience extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'inital_date',
+        'final_date',
+        'is_current',
+    ];
+
+    public function user(){
+        return $this->hasOne(User::class);
+    }
 }
